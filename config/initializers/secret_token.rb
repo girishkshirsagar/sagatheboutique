cecdc2eec3c3a34746d9badb8e3757b283a3efb3
girish <girish@workstation.(none)>
2014-01-10 14:47:58 +0530

    Girish - adding initial store repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MyStore::Application.config.secret_key_base = 'd6ed0e4407a0c8451a7318a0397d3998c429b688093449207abf826c44c91ee4eb09683acd20d1c3a14d398c01a84282703a79c6d95b715c876485822870a45b'
