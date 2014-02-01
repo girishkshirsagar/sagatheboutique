b6e353dc5c2ecee03721f2a44865510a0729ec48
girish <girish@workstation.(none)>
2014-02-01 17:57:13 +0530

    chaging routes as

--- app/gems/spree_multi_currency-2.0.0/config/routes.rb
+++ app/gems/spree_multi_currency-2.0.0/config/routes.rb
@@ -2,7 +2,7 @@
 
 # Put your extension routes here.
 Spree::Core::Engine.add_routes do
-  get 'currency/:id' => 'currency#set', as: :currency
+  get 'currency/:id' => 'currency#set', as: :currency_change
   namespace :admin do
     resources :currencies
     resources :currency_converters
